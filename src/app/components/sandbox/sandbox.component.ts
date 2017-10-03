b93081681fef6c20e0710cb1e4e2a80b491212cb
Rick Farina <rickrfarina@gmail.com>
2017-10-03 15:08:23 -0500

    mouse events

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -4,40 +4,57 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'sandbox',
     template:`
-    <!-- Using Pipes (formatting) -->
+    <!-- Events -->
     <h2>Hello from Sandbox</h2>
-    <p>My birthday is {{ birthday }}</p>
-    <p>My formatted birthday using date is {{ birthday | date }}</p>
-    <p>My formatted birthday using date:"MM-dd-yy" is {{ birthday | <p>My formatted birthday using date:"MM-dd-yyyy" is {{ birthday | date:"MM-dd-yyyy" }}</p>
-    
-    <hr>
-    <!-- Uppercase and Lowercase -->
-    <p>not formatted 'cake' I love {{ 'cake' }} </p>
-    <p>formatted 'cake' to uppercase:  I love {{ 'cake' | uppercase }} </p>
-    <p>formatted 'CAKE' to lowercase:  I love {{ 'CAKE' | lowercase }} </p>
+    <button id="btn" 
+        (click)="fireEvent($event)"
+        (mouseover)="fireEvent($event)"
+        (mousedown)="fireEvent($event)"
+        (mouseup)="fireEvent($event)"
+        (dblclick)="fireEvent($event)"
+        (drag)="fireEvent($event)"
+       
+        >Keyboard Event
+    </button>
 
-    <hr>
-    <!-- Currency -->
-    <p>You're unformatted total of {{ total }} is {{ total }} </p>
-    <p>You're formatted total of {{ total }} is   {{ total | currency }} </p>
-    <p>You're formatted total of {{ total }} in GBP is   {{ total | currency:"GBP" }} </p>
-    <p>You're formatted total of {{ total }} in GBP w/Symbol is   {{ total | currency:"GBP":"1" }} </p>
-    
-    <!-- Percent -->
-    <p>You're unformatted fee of {{ fee }} is {{ fee }} </p>
-    <p>You're formatted fee of {{ fee }} as % is {{ fee | percent }} </p>
-    
+    <!-- Drag Events do not seem to be firing! -->
+    <button id="btn2" 
+        (drag)="fireEvent($event)"
+        (click)="fireEvent($event)"
+        
+        >Drag Events
+    </button>
+
+
+
     `
 })
 
 export class SandboxComponent {
-
-    birthday = new Date(1981, 1, 15);
-    total: number = 500;
-    fee: number = 0.5;
+    
     constructor() {
-
+        
     }
+
+    fireEvent(e) {
+        e.preventDefault();
+        console.log('fire Event id: ', e.target.id);
+        console.log(e.type);
+
+        // switch (e.type) {
+        //     case 'click':
+        //         console.log('clicked');
+        //         break;
+        
+        //     case 'mouseover':
+        //         console.log('mouseover');
+        //         break;
+
+        //     default:
+        //         break;
+        // }        
+    }
+
 
 } // end class
 
