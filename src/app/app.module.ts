a78a136c50b30075bff30cc93970aede6ab59218
Rick Farina <rickrfarina@gmail.com>
2017-10-03 00:03:18 -0500

    @Component templateUrl, styles, styleUrls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SandboxComponent } from './components/sandbox.component';
+import { SandboxComponent } from './components/sandbox/sandbox.component';
 
 
 @NgModule({
---
+++ src/app/components/sandbox/sandbox.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+    selector: 'sandbox',
+    templateUrl: './sandbox.component.html',
+/*     styles: [`
+        .special {
+            color: green;
+            font-size: 20px;
+            text-transform: uppercase
+        }
+    `]
+ */
+        styleUrls: ['./sandbox.component.css'] 
+})
+
+export class SandboxComponent {
+
+} // end class
+
+
