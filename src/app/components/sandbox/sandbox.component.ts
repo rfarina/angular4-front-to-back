12b9703841fd959628dbc68e41395df9e362757c
Rick Farina <rickrfarina@gmail.com>
2017-10-03 00:22:28 -0500

    *ngFor on simple arrays and arrays of objects

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -3,20 +3,51 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'sandbox',
-    templateUrl: './sandbox.component.html',
-/*     styles: [`
-        .special {
-            color: green;
-            font-size: 20px;
-            text-transform: uppercase
-        }
-    `]
- */
-        styleUrls: ['./sandbox.component.css'] 
+    template:`
+        <h1>Hello World</h1>
+        {{people}}
+        <ul>
+            <li *ngFor="let person of people">
+                {{ person }}
+            </li>
+        </ul>
+        <ul>
+            <li *ngFor="let person of people; let i = index">
+            {{ i + 1 }} {{ person }}
+        </li>
+    </ul>
+
+    <h2>People</h2>
+    <ul>
+        <li *ngFor="let person of people2">
+        {{ person.firstName }}, {{ person.lastName }}
+    </li>
+</ul>
+
+
+`,
+    styleUrls: ['./sandbox.component.css'] 
 })
 
 export class SandboxComponent {
+    people = ['Rick', 'Daryl', 'Carl', 'Glen'];
+    people2 = [
+        {
+            firstName: 'Rick',
+            lastName: 'Farina'
+        },
+        {
+            firstName: 'John',
+            lastName: 'Doe'
+        },        {
+            firstName: 'Bob',
+            lastName: 'Smith'
+        }
+    ]
 
+    constructor() {
+        this.people[2] = 'Carol';
+    }
 } // end class
 
 
