1c66405369ed88fd6c9455c78d052521296be626
Rick Farina <rickrfarina@gmail.com>
2017-10-03 00:42:56 -0500

    conditional and switch

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -4,49 +4,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'sandbox',
     template:`
-        <h1>Hello World</h1>
-        {{people}}
-        <ul>
-            <li *ngFor="let person of people">
-                {{ person }}
-            </li>
-        </ul>
-        <ul>
-            <li *ngFor="let person of people; let i = index">
-            {{ i + 1 }} {{ person }}
-        </li>
-    </ul>
+    <h1>Hello 
 
-    <h2>People</h2>
-    <ul>
-        <li *ngFor="let person of people2">
-        {{ person.firstName }}, {{ person.lastName }}
-    </li>
-</ul>
+    <!-- Basic "if" on boolean
+       <span *ngIf="showName">{{ name }}</span>
+       <span *ngIf="!showName">World</span>
+    -->
+        <!-- Basic if else using ng-template -->
+        <span *ngIf="showName; else noName">{{ name }}</span>
+        <ng-template #noName>World</ng-template>
+ 
+        <!-- Basic if else using ternary operator -->
+        <p>Hello {{ showName ? name : 'World' }}</p>
 
+        <!-- Basic Switch -->
+        <hr>
 
-`,
+        <div [ngSwitch]="greeting">
+            <div *ngSwitchCase="'1'">Hello World</div>
+            <div *ngSwitchCase="'2'">Hi There</div>
+            <div *ngSwitchCase="'3'">What's up?</div>
+            <div *ngSwitchDefault>Hello</div>
+            </div>
+       </h1>`,
     styleUrls: ['./sandbox.component.css'] 
 })
 
 export class SandboxComponent {
-    people = ['Rick', 'Daryl', 'Carl', 'Glen'];
-    people2 = [
-        {
-            firstName: 'Rick',
-            lastName: 'Farina'
-        },
-        {
-            firstName: 'John',
-            lastName: 'Doe'
-        },        {
-            firstName: 'Bob',
-            lastName: 'Smith'
-        }
-    ]
+    name: string = 'John Doe';
+    showName: boolean = true;
+    greeting:string = '3';
 
     constructor() {
-        this.people[2] = 'Carol';
+
     }
 } // end class
 
