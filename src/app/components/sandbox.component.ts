f49f8e728336db67cb19a93796471164c766ec5e
Rick Farina <rickrfarina@gmail.com>
2017-10-02 19:39:31 -0500

    component class properties, methods, constructor, and interface

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'sandbox',
-    template: `<h1>Hello World from Sandbox</h1>`
+    template: `
+        <h1>{{ name }}, age {{ age }}, knows {{ person.firstName }} {{person.lastName}}</h1>
+        <h2>Person 2 is: {{person2.firstName}}, {{person2.lastName}}, who is {{person2.age}}</h2>
+        <ul>
+            <li>{{ showAge('current') }} </li>
+            <li>{{ showAge('nextbday') }} </li>
+            </ul>
+    
+    `
 })
 
-export class SandboxComponent{
+export class SandboxComponent implements OnInit   {
+    name: string = "John Doe";
+    age: number = 35;
+    person = {
+        firstName: 'Rick',
+        lastName: 'Farina'
+    }
+    person2: Person;
 
+    constructor() {
+        console.log('constructor ran', new Date().getTime());
+        this.hasBirthday();
+    }
+
+    ngOnInit() {
+        this.person2 = {
+            firstName: "Bob",
+            lastName: "Smith",
+            age: 45
+         }
+        
+         console.log('ngOnInit ran', new Date().getTime());
+         
+    } // end NgOnInit
+
+    hasBirthday(){
+        this.age++;
+    }
+
+    showAge(which:string) {
+        if(which === 'current'){
+            return this.age;
+        }else {
+            return ++this.age;
+        }
+    }
+
+} // end class
+
+
+interface Person {
+    firstName: string,
+    lastName: string,
+    age: number
 }
