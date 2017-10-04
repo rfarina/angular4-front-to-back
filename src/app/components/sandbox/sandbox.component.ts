9f185300d9504f7c3307f6f35924c23d4cffd3a3
Rick Farina <rickrfarina@gmail.com>
2017-10-03 19:37:08 -0500

    Template driven forms and validation

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -8,27 +8,74 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'sandbox',
     template:`
-    <!-- Form Submission -->
+    <!-- Template Driven Forms and Validation -->
     <h2>Hello from Sandbox</h2>
 
     <div class="container">
-        <form action="Post" (submit)="formSubmit()">
+        <form novalidate #f="ngForm" action="Post" (ngSubmit)="formSubmit(f)">
             <div class="form-group">
                 <label>Name</label>
                 <input 
                     type="text" 
                     class="form-control"
-                    [(ngModel)]="name" name="name"  >
+                    #userName="ngModel" 
+                    [(ngModel)]="user.name" name="name" 
+                    minlength="2" 
+                    required >
+            </div>
+            <!-- Name validations -->
+            <div 
+                *ngIf="userName.errors?.required && userName.touched"
+                class="alert alert-danger"    
+                >Name is required
             </div>
 
-            <div class="form-group">
-                <label>Age</label>
-                <input 
-                    type="number" 
-                    class="form-control"
-                    [(ngModel)]="age" name="age"
-                    >
+            <div 
+                *ngIf="userName.errors?.minlength && userName.touched"
+                class="alert alert-danger"    
+                >Name should be at least 2 characters
             </div>
+
+
+
+
+            <div class="form-group">
+                <label>Email</label>
+                <input 
+                type="text" 
+                class="form-control"
+                #userEmail="ngModel" 
+                [(ngModel)]="user.email" name="email" 
+                required >
+            </div>
+            <!-- Email validations -->
+            <div 
+                *ngIf="userEmail.errors?.required && userName.touched"
+                class="alert alert-danger"    
+                >Email is required
+            </div>
+
+
+
+
+            <div class="form-group">
+                <label>Phone</label>
+                <input 
+                type="phone" 
+                class="form-control"
+                #userPhone="ngModel" 
+                [(ngModel)]="user.phone" name="phone" 
+                minlength="10">
+            </div>
+            <!-- Phone validations -->
+            <div 
+                *ngIf="userPhone.errors?.minlength && userPhone.touched"
+                class="alert alert-danger"    
+                >Phone must be at least 10 characters
+            </div>
+
+
+
             <input type="submit" value="Submit" class="btn btn-success">
 
             <!-- Add to list when form is submitted -->
@@ -40,24 +87,29 @@ import { Component } from '@angular/core';
             </ul>
             </form>
 
-        <h4>{{ name }}</h4>
-        <h4>{{ age }}</h4>
         </div>
 
     `
 })
 
 export class SandboxComponent {
-    name: string = "Bob";  // two way bound
-    age: number = 25;      // two way bound 
     names:string[] = ['Bob', 'John', 'Sally'];
+    user = {
+        name: '',
+        email: '',
+        phone: ''
+    }
     constructor() {
     }
     
-    formSubmit() {
+    formSubmit({value, valid}) {
         console.log('form submitted');
-        this.names.push(this.name);
-        this.name = '';
+        if(valid) {
+            console.log('valid data: ', value);
+            this.names.push(this.user.name);            
+        } else {
+            console.log('invalid data: ', value);
+        }
     }
 
 } // end class
