08b8e1b4eb8dea5add3cee0ca5a1946e654aced5
Rick Farina <rickrfarina@gmail.com>
2017-10-09 18:34:37 -0500

    observables in UI and Services

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -1,29 +1,82 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../services/data.service';
+import { Subscriber } from 'rxjs/Subscriber';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/of';
+import * as Rx from 'rxjs';
 
 
 
 @Component({
     selector: 'sandbox',
     template:`
-    <!-- Using Service -->
+    <!-- Using Service that returns an Observable -->
     <h2>Hello from Sandbox</h2>
     <ul class="list-group">
-        <li class="list-group-item" *ngFor="let user of users">{{ user }}</li>
+        <li class="list-group-item" *ngFor="let d of data">{{ d }}</li>
     </ul>
+    <hr>
+    <button (click)="getNumbers()">Get Numbers</button>
     `
 })
 
 export class SandboxComponent {
-    users:string[];
+    data:any[] = [];
+    names:string[] = ['Callie', 'Rick'];
 
-    constructor(dataService: DataService) {
-        this.users = dataService.getUsers();
-        console.log('users', this.users);
+
+    constructor(public dataService: DataService) {
+        console.log('before');
+        var subscription = dataService.getData()       // returns observable
+        .subscribe(
+            (data) => {
+                console.log(data);
+                this.data.push(data);
+            },
+            (err) => {
+                console.log(err);
+                subscription.unsubscribe();
+            },
+            () => {
+                subscription.unsubscribe();
+            }
+        
+        ); // end subscribe
+        console.log('after');
+
+        this.getClicksOnDocument();
+
+        this.getNames();
+    } // end constructor
+    
+    getNumbers() {
+        this.dataService.getNumbers()
+        .subscribe(
+            (value:number) => console.log(`value is ${value}`),
+            
+            (err) => console.log(`Error: ${err}`),
+
+            () => {console.log('complete')}
+        )
+    }
+
+    getClicksOnDocument() {
+        let clicks = Observable.fromEvent(document, 'click');
+        clicks.subscribe(
+            (evt) => console.log(evt)
+        );
+
 
     }
-    
 
+    getNames(){
+        let names = Observable.of(this.names);
+        names.subscribe(
+            (name) => console.log(name)
+        )
+    }
 } // end class
 
 
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,17 +1,92 @@
+/* 
+    Objective: Create an observable and return our data
+    as a stream. 
+*/
+
 import { Injectable } from '@angular/core';
+import { Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/count';
+import 'rxjs/add/operator/filter';
+
 
 
 @Injectable()
 export class DataService{
-    users:string[];
+    data: Observable<Array<any>>;
+    numbers: number[] = [1, 2, 3, 4, 5, 6, 7];
 
     constructor() {
-        this.users = ['Mark', 'Sharon', 'Beth', 'John'];
 
     }
 
-    getUsers() {
-        return this.users;
+    /* 
+        This function will return data every second except for
+        the first push of data, which will be synchronous. We will 
+        implement this by creating an observable, and using a 
+        timer to return the asynch data over a period of 5 seconds,
+        when the observable completes.
+    */
+    getData() {
+        this.data = new Observable(observer => {
+            observer.next(['here is your synchronous data']);
+            setTimeout(() => {
+                observer.next([1]);
+            }, 1000);
+
+            setTimeout(() => {
+                observer.next([2]);
+            }, 1000);
+
+            setTimeout(() => {
+                observer.next([3]);
+            }, 1000);
+
+            setTimeout(() => {
+                observer.next(['hello']); // this should generate an error for the subscriber
+            }, 1000);
+
+            setTimeout(() => {
+                observer.complete();
+            }, 1000);
+
+        })        
+        return this.data;
+        
     }
+
+    getNumbers() {
+
+        let source = Observable.create(observer => {
+            let index = 0;
+            let produceValue = () => {
+                observer.next(this.numbers[index++]);
+
+                if(index === 30000) {
+                    observer.error('Something went wrong');
+                }
+                if(index < this.numbers.length) {
+                    setTimeout(produceValue, 250);
+                }else {
+                    observer.complete();
+                }
+            }
+
+            // Invoke the function
+            produceValue();
+
+        })
+        .map(n => n *2)
+        .filter(n => n >= 6)
+        .count();
+        // .filter(n => n === 6);
+        
+
+        // return the observable
+        return source;
+    }
+
+
+
 
 } // end class
